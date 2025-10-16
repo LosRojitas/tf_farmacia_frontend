0bebd04a12d005f9f0c33de39b7184e06aad2b53
Anthon Joss <jossue.ramirezanthony@gmail.com>
2025-10-15 23:23:49 -0500

    correcciones para nube

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './pages/login/login.js'; // lo que ya tienes
 
 function boot() {
   const hook = document.getElementById('products-view');
-  if (hook) import('./pages/products/products.js').then(m => m.initProducts());
+  if (hook) import('/tf_farmacia_frontend/pages/products/products.js').then(m => m.initProducts());
 }
 
 if (document.readyState === 'loading') {
